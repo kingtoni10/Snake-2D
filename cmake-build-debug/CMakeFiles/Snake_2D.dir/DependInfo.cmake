d0c2bce62294e805962a495cb7d2a20fee288eac
kingtoni10 <marc.toni15.3@gmail.com>
2022-11-08 18:04:41 +0100

    first test commit

---
+++ cmake-build-debug/CMakeFiles/Snake_2D.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/aUlm/semesters/sem 5 WS 2022/computer graphics/Snake 2D/main.cpp" "D:/aUlm/semesters/sem 5 WS 2022/computer graphics/Snake 2D/cmake-build-debug/CMakeFiles/Snake_2D.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Snake_2D.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Snake_2D.dir/main.cpp.obj"
+  "Snake_2D.exe"
+  "Snake_2D.exe.manifest"
+  "Snake_2D.pdb"
+  "libSnake_2D.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Snake_2D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
